463093ff687dbb199393215e9dbc036e5286a15b
Taush Sampley <taushsampley@gmail.com>
2022-03-22 19:34:19 -0400

    Rough out compose UI

--- app/src/main/java/com/taushsampley/timer/MainActivity.kt
+++ app/src/main/java/com/taushsampley/timer/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.taushsampley.timer
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -17,27 +19,76 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             TimerTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+
+                TaskUi()
+                /*
+                TODO:
+                  1. Current Task Timer
+                  2. Task Time List (Task Title : Time)
+                  3. New Task Title (Input Box)
+                  4. Start/Pause/New (Button)
+                 */
             }
         }
     }
 }
 
+/**
+ * Elapsed time for current active task
+ * TODO: mock out
+ *   1. Elapsed Time Text, centered
+ *   2. Backing Circle
+ *   3. Seconds "Hand" border
+ */
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun TaskTimer() {
+
+}
+
+/**
+ * List of prior task times
+ * TODO: mock out
+ *   1. Lazy Column
+ *   2. Populate timers
+ */
+@Composable
+fun TaskList() {
+
+}
+
+/**
+ * New task creation input/controls
+ * TODO: mock out
+ *   1. Text Input
+ *   2. Control button
+ */
+@Composable
+fun TaskControl() {
+
+}
+
+/**
+ * Full task timer UI
+ */
+@Composable
+fun TaskUi() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colors.background
+    ) {
+        Column {
+            TaskTimer()
+            TaskList()
+            TaskControl()
+        }
+    }
 }
 
 @Preview(showBackground = true)
+@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
 fun DefaultPreview() {
     TimerTheme {
-        Greeting("Android")
+        TaskUi()
     }
 }
