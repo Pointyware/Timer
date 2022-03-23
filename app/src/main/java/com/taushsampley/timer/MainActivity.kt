66322c47af151dcaf9954cb2743ddf82ecdf32d8
Taush Sampley <taushsampley@gmail.com>
2022-03-22 20:38:28 -0400

    Complete `TaskList` composable function
    Use dummy data to fill in list

--- app/src/main/java/com/taushsampley/timer/MainActivity.kt
+++ app/src/main/java/com/taushsampley/timer/MainActivity.kt
@@ -4,14 +4,17 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.taushsampley.timer.ui.theme.TimerTheme
 
 class MainActivity : ComponentActivity() {
@@ -47,13 +50,39 @@ fun TaskTimer() {
 
 /**
  * List of prior task times
- * TODO: mock out
- *   1. Lazy Column
- *   2. Populate timers
  */
 @Composable
 fun TaskList() {
+    val currentTime = System.currentTimeMillis()
+    val taskList = listOf(
+        Task("programming", currentTime, currentTime + 3641000),
+        Task("cleaning", currentTime, currentTime + 7223000),
+        Task("eating", currentTime, currentTime + 50000)
+    ) // TODO: pull from actual data source/view model
 
+    LazyColumn(modifier = Modifier.fillMaxWidth()) {
+        items(taskList) { task ->
+
+            Surface(
+                modifier = Modifier
+                    .padding(all = 8.dp),
+                elevation = 2.dp,
+                shape = MaterialTheme.shapes.medium
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth().padding(all = 4.dp),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Text(text = task.title, style = MaterialTheme.typography.body1)
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Text(
+                        text = TaskFormatter.durationStringForTask(task),
+                        style = MaterialTheme.typography.body1
+                    )
+                }
+            }
+        }
+    }
 }
 
 /**
