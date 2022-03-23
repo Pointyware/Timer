f1206c2ab2cd7a1bafdfb15ff189befcba7ff345
Taush Sampley <taushsampley@gmail.com>
2022-03-22 21:07:54 -0400

    Add timer controls at bottom of screen

--- app/src/main/java/com/taushsampley/timer/MainActivity.kt
+++ app/src/main/java/com/taushsampley/timer/MainActivity.kt
@@ -9,13 +9,11 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -103,13 +101,22 @@ fun TaskList() {
 
 /**
  * New task creation input/controls
- * TODO: mock out
- *   1. Text Input
- *   2. Control button
  */
 @Composable
 fun TaskControl() {
-
+    // TODO: bind and update with view model
+    var text by remember { mutableStateOf("") }
+    TextField(
+        value = text,
+        onValueChange = { text = it },
+        label = { Text("Task Title") }
+    )
+    Button(
+        // TODO: notify view model of creation
+        onClick = { println("Start timing new task") }
+    ) {
+        Text(text = stringResource(R.string.start_task))
+    }
 }
 
 /**
@@ -120,9 +127,17 @@ fun TaskUi() {
     Surface(
         color = MaterialTheme.colors.background
     ) {
-        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxHeight()
+        ) {
             TaskTimer()
             TaskList()
+            /*
+             TODO:
+               1. attach input/controls to bottom of page
+               2. create space between list and controls
+             */
             TaskControl()
         }
     }
