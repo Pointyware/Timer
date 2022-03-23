8abf6abae2071635e9889373f9402a3116525c93
Taush Sampley <taushsampley@gmail.com>
2022-03-22 20:23:39 -0400

    Add start and end time properties to `Task`

--- app/src/main/java/com/taushsampley/timer/Task.kt
+++ app/src/main/java/com/taushsampley/timer/Task.kt
@@ -1,12 +1,22 @@
 package com.taushsampley.timer
 
+import java.util.concurrent.TimeUnit
+
 /**
  * Describes a prior tracked task.
  *
+ * Clean Layer: Entity
+ *
  * @property title The name of the task.
  * @property elapsedTime The amount of time recorded for the task in seconds.
+ * @property startTime The start time of the task, in milliseconds, using Unix epoch.
+ * @property endTime The end time of the task, in milliseconds, using Unix epoch.
  */
 data class Task(
     val title: String,
+    val startTime: Long,
+    val endTime: Long
+) {
     val elapsedTime: Int
-)
+        get() = TimeUnit.MILLISECONDS.toSeconds(endTime - startTime).toInt()
+}
