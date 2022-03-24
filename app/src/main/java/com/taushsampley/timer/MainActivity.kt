eab0bdb6f2a27af7131f257a743cc713d5a8d726
Taush Sampley <taushsampley@gmail.com>
2022-03-24 11:45:27 -0400

    Adjust task timer
    * Replace background circle with timer arc
    * Shrink elapsed time display

--- app/src/main/java/com/taushsampley/timer/MainActivity.kt
+++ app/src/main/java/com/taushsampley/timer/MainActivity.kt
@@ -4,17 +4,20 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.taushsampley.timer.ui.theme.TimerTheme
@@ -47,17 +50,30 @@ class MainActivity : ComponentActivity() {
  */
 @Composable
 fun TaskTimer() {
-    // TODO: make circle and text smaller
     Box(
         contentAlignment = Alignment.Center,
         modifier = Modifier
-            .background(MaterialTheme.colors.secondary, CircleShape)
-            .aspectRatio(1f)
+            .padding(horizontal = 24.dp, vertical = 8.dp)
     ) {
+
+        val arcColor = MaterialTheme.colors.primary
+        Canvas(modifier = Modifier.aspectRatio(1f)) {
+
+            drawArc(
+                color = arcColor,
+                startAngle = -90f,
+                sweepAngle = 270f,
+                useCenter = false,
+                style = Stroke(width = 8f, cap = StrokeCap.Round)
+            )
+        }
+
         Text(
             text = "00:00:00",
-            textAlign = TextAlign.Center,
-            style = MaterialTheme.typography.h1
+//            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.h4,
+            modifier = Modifier
+                .align(Alignment.Center)
         )
     }
 }
