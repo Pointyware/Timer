19a1b05b07b98310207b3837a72700becfa03af8
Taush Sampley <taushsampley@gmail.com>
2022-04-02 06:33:04 -0400

    Swap Row for BottomAppBar in bottom of scaffold

--- app/src/main/java/com/taushsampley/timer/MainActivity.kt
+++ app/src/main/java/com/taushsampley/timer/MainActivity.kt
@@ -3,19 +3,20 @@ package com.taushsampley.timer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.taushsampley.timer.calendar.CalendarScreen
 import com.taushsampley.timer.metrics.MetricsScreen
@@ -65,7 +66,7 @@ fun TimerApp() {
                 )
             },
             bottomBar = {
-                Row {
+                BottomAppBar {
                     allScreens.forEach {
                         Button(onClick = { navController.navigate(it.name) }) {
                             Text(it.name)
