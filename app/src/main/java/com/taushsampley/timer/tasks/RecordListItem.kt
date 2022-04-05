d1813721a7705ba458c5341595f58deb10a50a64
Taush Sampley <taushsampley@gmail.com>
2022-04-05 04:54:40 -0400

    Replace use of `Record` entity in composables with `RecordListItem`

---
+++ app/src/main/java/com/taushsampley/timer/tasks/RecordListItem.kt
@@ -0,0 +1,11 @@
+package com.taushsampley.timer.tasks
+
+/**
+ * Holds the information relevant to displaying a particular record.
+ *
+ * @property duration The duration of the record in seconds formatted by [RecordFormatter].
+ */
+data class RecordListItem(
+    val title: String,
+    val duration: String
+)
