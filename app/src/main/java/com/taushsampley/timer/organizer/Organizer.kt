23d26beffbb2ef05f54e9313dd33f97e84dee112
Taush Sampley <taushsampley@gmail.com>
2022-04-05 05:00:15 -0400

    Create organizer composables

---
+++ app/src/main/java/com/taushsampley/timer/organizer/Organizer.kt
@@ -0,0 +1,80 @@
+package com.taushsampley.timer.organizer
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.clearAndSetSemantics
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.taushsampley.timer.R
+import com.taushsampley.timer.ui.theme.TimerTheme
+
+/**
+ * Presents a list of hierarchical elements as described by [elements].
+ *
+ * @param elements The hierarchy elements to display.
+ * @param onClickElement When a user clicks on an organizer element, will receive the element and
+ * its index.
+ */
+@Composable
+fun Organizer(
+    elements: List<OrganizerElement>,
+    onClickElement: (OrganizerElement, Int) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    
+    LazyColumn {
+        items(elements) {
+            OrganizerRow(it)
+        }
+    }
+}
+
+/**
+ * Presents the information about a particular element. Depth is expressed as left-spacing, an icon
+ * is used to show if the element is a branch or leaf node, and the title is displayed as text.
+ */
+@Composable
+fun OrganizerRow(element: OrganizerElement) {
+
+    val typeString = if (element.type == OrganizerElement.Type.Branch)
+        stringResource(R.string.acc_name_category)
+    else stringResource(R.string.acc_name_task)
+
+    Row(
+        modifier = Modifier.clearAndSetSemantics {
+            contentDescription = "$typeString ${element.title}"
+        }
+    ) {
+        Spacer(modifier = Modifier.width((8*element.level).dp))
+        Icon(painter = painterResource(R.drawable.ic_category), contentDescription = null)
+        Text(text = element.title)
+    }
+}
+
+@Preview
+@Composable
+fun OrganizerPreview() {
+    val elementList = listOf(
+        OrganizerElement("alpha", OrganizerElement.Type.Branch),
+        OrganizerElement("beta", OrganizerElement.Type.Branch, 1),
+        OrganizerElement("gamma", OrganizerElement.Type.Branch, 2),
+        OrganizerElement("delta", OrganizerElement.Type.Leaf, 3),
+        OrganizerElement("epsilon", OrganizerElement.Type.Leaf, 3)
+    )
+
+    TimerTheme {
+        Organizer(elements = elementList, onClickElement = {_, _ -> })
+    }
+}
