7a73dafddae8d61f6b91d5cdd70e8647105b243e
Taush Sampley <taushsampley@gmail.com>
2022-04-05 07:13:16 -0400

    Implement basic timing functionality

--- app/src/main/java/com/taushsampley/timer/tasks/TimerViewModel.kt
+++ app/src/main/java/com/taushsampley/timer/tasks/TimerViewModel.kt
@@ -1,8 +1,12 @@
 package com.taushsampley.timer.tasks
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
 class TimerViewModel: ViewModel() {
 
@@ -12,7 +16,28 @@ class TimerViewModel: ViewModel() {
     private val _isRunning = MutableStateFlow(false)
     val isRunning: StateFlow<Boolean> = _isRunning
 
+    private var timerJob: Job? = null
+
     fun toggleTimer() {
-        _isRunning.value = !_isRunning.value
+        val isRunning = !_isRunning.value
+        _isRunning.value = isRunning
+
+        if (isRunning) {
+            timerJob = viewModelScope.launch {
+                while (true) {
+                    delay(1000)
+                    _timer.value++
+                }
+            }
+
+            val startTime = System.currentTimeMillis()
+            // TODO: get task title and record start of time
+        } else {
+            timerJob?.cancel()
+            _timer.value = 0
+
+            val endTime = System.currentTimeMillis()
+            // TODO: complete record with end time
+        }
     }
 }
