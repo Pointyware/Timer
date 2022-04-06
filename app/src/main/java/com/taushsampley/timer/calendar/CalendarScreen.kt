8dde7dc1794647c558fcf0f045267075146d44eb
Taush Sampley <taushsampley@gmail.com>
2022-04-06 02:00:06 -0400

    Replace `CalendarScreen` placeholder content with actual calendar

--- app/src/main/java/com/taushsampley/timer/calendar/CalendarScreen.kt
+++ app/src/main/java/com/taushsampley/timer/calendar/CalendarScreen.kt
@@ -1,11 +1,39 @@
 package com.taushsampley.timer.calendar
 
-import androidx.compose.material.Text
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.tooling.preview.Preview
+import com.taushsampley.timer.ui.theme.TimerTheme
 
 /**
  */
 @Composable
 fun CalendarScreen(calendarViewModel: CalendarViewModel) {
-    Text("Calendar")
+
+    Column {
+
+//        val month by calendarViewModel.month.collectAsState()
+//        val year by calendarViewModel.year.collectAsState()
+        val metrics by calendarViewModel.metrics.collectAsState()
+
+        Calendar(metrics)
+
+
+    }
+
+}
+
+@Preview(showBackground = true,)
+@Preview(showBackground = true, uiMode = UI_MODE_NIGHT_YES)
+@Composable
+fun CalendarScreenPreview() {
+
+    val viewModel = CalendarViewModel()
+
+    TimerTheme {
+        CalendarScreen(viewModel)
+    }
 }
