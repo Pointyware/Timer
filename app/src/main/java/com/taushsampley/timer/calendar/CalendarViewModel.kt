1c5ec50c04c516054a00391373b08848548a18c3
Taush Sampley <taushsampley@gmail.com>
2022-04-06 01:59:15 -0400

    Add state and event properties to `CalendarViewModel`

--- app/src/main/java/com/taushsampley/timer/calendar/CalendarViewModel.kt
+++ app/src/main/java/com/taushsampley/timer/calendar/CalendarViewModel.kt
@@ -1,7 +1,32 @@
 package com.taushsampley.timer.calendar
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
 class CalendarViewModel: ViewModel() {
 
+    private val _metrics = MutableStateFlow<MonthMetrics?>(null)
+    val metrics: StateFlow<MonthMetrics?> = _metrics
+
+    /**
+     * Load metrics data for the given month in the given year.
+     */
+    fun selectMonthAndYear(month: Int, year: Int) {
+        TODO("request data for given month and year")
+    }
+
+    /**
+     * Load metrics data for month before the current metrics.
+     */
+    fun goToPreviousMonth() {
+        TODO("request data for previous month")
+    }
+
+    /**
+     * Load metrics data for month after the current metrics.
+     */
+    fun goToNextMonth() {
+        TODO("request data for next month")
+    }
 }
