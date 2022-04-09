716f1f3aa02a36eb92e1c6d862856732e3b515ac
Taush Sampley <taushsampley@gmail.com>
2022-04-09 07:04:00 -0400

    Define data access functions on `TaskRepository`

--- app/src/main/java/com/taushsampley/timer/tasks/data/TaskRepository.kt
+++ app/src/main/java/com/taushsampley/timer/tasks/data/TaskRepository.kt
@@ -1,8 +1,52 @@
 package com.taushsampley.timer.tasks.data
 
+import com.taushsampley.timer.tasks.Category
+import com.taushsampley.timer.tasks.Node
+import com.taushsampley.timer.tasks.Record
+import com.taushsampley.timer.tasks.Task
+
 /**
- * Contract definition for objects that mediate between multiple data sources.
+ * Contract definition for objects that mediate between multiple task data sources.
+ *
+ * I'm attempting to simplify interactions by making entities immutable and using CQRS in this
+ * interface.
+ *
+ * Clean Layer: Interactors – implemented in outer layers
  */
 interface TaskRepository {
 
+    suspend fun getRootNodes(): List<Node>
+
+    // region Category access
+    suspend fun addCategory(category: Category, parent: Category?)
+
+    suspend fun getCategoriesIn(category: Category?): List<Category>
+
+    suspend fun renameCategory(category: Category, newTitle: String)
+    suspend fun moveCategory(category: Category, newParent: Category?)
+
+    suspend fun removeCategory(category: Category)
+    // endregion
+
+    // region Task access
+    suspend fun addTask(task: Task, category: Category?)
+
+    suspend fun getTasksIn(category: Category?): List<Task>
+
+    suspend fun renameTask(task: Task, newTitle: String)
+    suspend fun moveTask(task: Task, newCategory: Category?)
+
+    suspend fun removeTask(task: Task)
+    // endregion
+
+    // region Record access
+    suspend fun addRecord(record: Record, task: Task)
+
+    suspend fun getRecordsIn(task: Task): List<Record>
+
+    suspend fun editRecord(task: Record, start: Long, end: Long)
+    suspend fun moveRecord(record: Record, task: Task)
+
+    suspend fun removeRecord(record: Record)
+    // endregion
 }
