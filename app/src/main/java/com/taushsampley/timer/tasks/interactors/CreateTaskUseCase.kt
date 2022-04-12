b2f6c396e59b18d585ffc26a6128e87ff5cba4ed
Taush Sampley <taushsampley@gmail.com>
2022-04-12 05:28:13 -0400

    Implement Create*UseCase

--- app/src/main/java/com/taushsampley/timer/tasks/interactors/CreateTaskUseCase.kt
+++ app/src/main/java/com/taushsampley/timer/tasks/interactors/CreateTaskUseCase.kt
@@ -1,7 +1,25 @@
 package com.taushsampley.timer.tasks.interactors
 
+import com.taushsampley.timer.tasks.Task
+import com.taushsampley.timer.tasks.data.TaskRepository
+import java.sql.SQLDataException
+
 /**
- * TODO: describe purpose/intent of CreateNewTaskUseCase
  */
-class CreateTaskUseCase {
+class CreateTaskUseCase(
+    private val repository: TaskRepository
+) {
+    suspend operator fun invoke(taskTitle: String): Result {
+        return try {
+            repository.addTask(taskTitle)
+            Result.Success(Task(taskTitle))
+        } catch (duplicateException: SQLDataException) {
+            Result.Error(duplicateException)
+        }
+    }
+
+    sealed interface Result {
+        class Error(val throwable: Throwable): Result
+        class Success(val newTask: Task): Result
+    }
 }
