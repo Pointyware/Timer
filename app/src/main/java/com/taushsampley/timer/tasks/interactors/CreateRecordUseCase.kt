5aeefb1b38cc0ca88eb113d885464d50e921f2e8
Taush Sampley <taushsampley@gmail.com>
2022-04-12 05:28:13 -0400

    Implement Create*UseCase

--- app/src/main/java/com/taushsampley/timer/tasks/interactors/CreateRecordUseCase.kt
+++ app/src/main/java/com/taushsampley/timer/tasks/interactors/CreateRecordUseCase.kt
@@ -1,7 +1,24 @@
 package com.taushsampley.timer.tasks.interactors
 
+import com.taushsampley.timer.tasks.Record
+import com.taushsampley.timer.tasks.Task
+import com.taushsampley.timer.tasks.data.TaskRepository
+
 /**
- * TODO: describe purpose/intent of CreateRecordUseCase
  */
-class CreateRecordUseCase {
+class CreateRecordUseCase(
+    private val repository: TaskRepository,
+    private val createTaskUseCase: CreateTaskUseCase
+) {
+    suspend operator fun invoke(record: Record, task: Task) {
+        repository.addRecord(record, task)
+    }
+
+    suspend operator fun invoke(record: Record, taskTitle: String): CreateTaskUseCase.Result {
+        val result = createTaskUseCase(taskTitle)
+        if (result is CreateTaskUseCase.Result.Success) {
+            this(record, result.newTask)
+        }
+        return result
+    }
 }
