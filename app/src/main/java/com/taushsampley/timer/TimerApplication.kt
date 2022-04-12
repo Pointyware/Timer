5c020cee710496ad7d9fc80690f771069cb07524
Taush Sampley <taushsampley@gmail.com>
2022-04-12 05:27:38 -0400

    initialize database and repository in `Application.onCreate`

--- app/src/main/java/com/taushsampley/timer/TimerApplication.kt
+++ app/src/main/java/com/taushsampley/timer/TimerApplication.kt
@@ -1,11 +1,28 @@
 package com.taushsampley.timer
 
 import android.app.Application
+import androidx.room.Room
+import com.taushsampley.timer.tasks.data.RoomTaskRepository
+import com.taushsampley.timer.tasks.data.TaskDatabase
+import com.taushsampley.timer.tasks.data.TaskRepository
 
 /**
  *
  */
 class TimerApplication: Application() {
 
+    private lateinit var database: TaskDatabase
 
+    lateinit var repository: TaskRepository
+
+    override fun onCreate() {
+        super.onCreate()
+
+        database = Room.databaseBuilder(
+            this,
+            TaskDatabase::class.java, "timer-db"
+        ).build()
+
+        repository = RoomTaskRepository(database)
+    }
 }
