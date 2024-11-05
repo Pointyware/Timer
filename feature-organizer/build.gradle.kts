725966701f294240a58cbbbd71c071057a97d6ad
Taush Sampley <taushsampley@gmail.com>
2024-11-04 21:11:15 -0600

    Copy multiplatform scripts to other feature modules

---
+++ feature-organizer/build.gradle.kts
@@ -0,0 +1,72 @@
+
+plugins {
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.composeMultiplatform)
+    alias(libs.plugins.compose.compiler)
+}
+
+kotlin {
+    jvmToolchain(17)
+
+    jvm("desktop")
+    androidTarget()
+    js {
+        browser {
+            binaries.executable()
+        }
+    }
+
+    applyDefaultHierarchyTemplate()
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+
+            }
+        }
+
+        val commonTest by getting {
+
+        }
+
+        val jvmSharedMain by creating {
+
+        }
+
+        val jvmSharedTest by creating {
+
+        }
+
+        val desktopMain by getting {
+            dependsOn(jvmSharedMain)
+
+            dependencies {
+
+            }
+        }
+
+        val desktopTest by getting {
+
+        }
+
+        val androidMain by getting {
+            dependsOn(jvmSharedMain)
+
+            dependencies {
+
+            }
+        }
+
+        val androidUnitTest by getting {
+
+        }
+        val androidInstrumentedTest by getting {
+
+        }
+    }
+}
+
+android {
+    namespace = "org.pointyware.timer.organizer"
+}
