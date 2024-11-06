161f4a3ba20a138e9e79e8991f43d169ecf58953
Taush Sampley <taushsampley@gmail.com>
2024-11-05 18:40:25 -0600

    Add :core-ui module

---
+++ core-ui/build.gradle.kts
@@ -0,0 +1,73 @@
+
+plugins {
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.composeMultiplatform)
+    alias(libs.plugins.compose.compiler)
+}
+
+kotlin {
+    jvmToolchain(17)
+
+    jvm("desktop")
+    androidTarget()
+    js {
+        browser {
+            binaries.executable()
+        }
+    }
+
+    applyDefaultHierarchyTemplate()
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+
+            }
+        }
+
+        val commonTest by getting {
+
+        }
+
+        val jvmSharedMain by creating {
+
+        }
+
+        val jvmSharedTest by creating {
+
+        }
+
+        val desktopMain by getting {
+            dependsOn(jvmSharedMain)
+
+            dependencies {
+
+            }
+        }
+
+        val desktopTest by getting {
+
+        }
+
+        val androidMain by getting {
+            dependsOn(jvmSharedMain)
+
+            dependencies {
+
+            }
+        }
+
+        val androidUnitTest by getting {
+
+        }
+        val androidInstrumentedTest by getting {
+
+        }
+    }
+}
+
+android {
+    namespace = "org.pointyware.timer.ui"
+    compileSdk = 35
+}
