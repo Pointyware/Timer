9f5452716c927f58b8d541b4c117da08adf2b72b
Taush Sampley <taushsampley@gmail.com>
2024-11-17 15:54:00 -0600

    Create SqlDelight task repo implementation to replace room impl

---
+++ app-shared/src/commonMain/kotlin/org/pointyware/timer/shared/data/TaskRepositoryImpl.kt
@@ -0,0 +1,66 @@
+package org.pointyware.timer.shared.data
+
+import org.pointyware.timer.data.TaskRepository
+import org.pointyware.timer.entities.Record
+import org.pointyware.timer.entities.Task
+import org.pointyware.timer.shared.db.TimerDatabase
+import org.pointyware.timer.shared.local.DriverFactory
+import org.pointyware.timer.shared.local.Persistence
+import org.pointyware.timer.shared.local.createOrMigrate
+
+/**
+ *
+ */
+class TaskRepositoryImpl(
+    private val driverFactory: DriverFactory,
+    private val persistence: Persistence = Persistence.File
+): TaskRepository {
+
+    private val driver = driverFactory.createSqlDriver(persistence)
+    private val db by lazy {
+        TimerDatabase.Schema.createOrMigrate(driver)
+        TimerDatabase(driver)
+    }
+
+    override suspend fun addTask(title: String): Task {
+        db.tasksQueries.insertTask(title)
+        val taskDto = db.tasksQueries.getTask(title).executeAsOne()
+        return Task(taskDto.title, taskDto.id)
+    }
+
+    override suspend fun getTaskByTitle(title: String): Result<Task> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getTasks(): List<Task> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun renameTask(task: Task, newTitle: String) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun removeTask(task: Task) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun addRecord(record: Record, task: Task): Result<Record> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getRecordsIn(task: Task): List<Record> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun editRecord(record: Record, start: Long, end: Long) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun moveRecord(record: Record, task: Task) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun removeRecord(record: Record) {
+        TODO("Not yet implemented")
+    }
+}
---
+++ app-shared/src/commonMain/kotlin/org/pointyware/timer/shared/local/SqlDelightExt.kt
@@ -0,0 +1,42 @@
+package org.pointyware.timer.shared.local
+
+import app.cash.sqldelight.db.QueryResult
+import app.cash.sqldelight.db.SqlDriver
+import app.cash.sqldelight.db.SqlSchema
+
+/**
+ * Extension property to get and set the version of the database.
+ */
+var SqlDriver.version: Long
+    get() = executeQuery(
+        identifier = null,
+        sql = "PRAGMA user_version;",
+        mapper = { query ->
+            query.next() // Android Driver does not prime the cursor
+            QueryResult.Value(query.getLong(0) ?: 0L)
+        },
+        parameters = 0,
+        binders = null,
+    ).value
+    set(value) {
+        execute(
+            identifier = null,
+            sql = "PRAGMA user_version = $value;",
+            parameters = 0,
+            binders = null
+        )
+    }
+
+/**
+ * Extension function to create or migrate the database schema.
+ */
+fun <T : QueryResult<Unit>> SqlSchema<T>.createOrMigrate(driver: SqlDriver) {
+    val lastVersion = driver.version
+    val newVersion = version
+    if (lastVersion == 0L) {
+        create(driver)
+        driver.version = newVersion
+    } else {
+        migrate(driver, lastVersion, newVersion)
+    }
+}
