40815313826181b003bcff186a9ea61217c89489
Taush Sampley <taushsampley@gmail.com>
2024-11-17 17:30:35 -0600

    create android unit test create record use case unit test

---
+++ app-shared/src/androidUnitTest/kotlin/org/pointyware/timer/shared/interactors/CreateRecordUseCaseTest.kt
@@ -0,0 +1,8 @@
+package org.pointyware.timer.shared.interactors
+
+/**
+ *
+ */
+class CreateRecordUseCaseTest {
+    // TODO: experiment using expected/actual classes for platform-specific setup/teardown with shared test cases
+}
