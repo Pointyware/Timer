13ef956f3f39e09ebb5f69600f3385b9e6447b2c
Taush Sampley <taushsampley@gmail.com>
2024-11-17 18:08:10 -0600

    explicitly enable edge-to-edge for devices below 35

--- app-android/src/main/java/org/pointyware/timer/MainActivity.kt
+++ app-android/src/main/java/org/pointyware/timer/MainActivity.kt
@@ -3,6 +3,7 @@ package org.pointyware.timer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.BottomAppBar
@@ -50,6 +51,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
             TimerApp(timerViewModel, organizerViewModel, calendarViewModel, metricsViewModel)
         }
