121d8ab83cce0a2e0a38adc23022248b2f1315e7
Taush Sampley <taushsampley@gmail.com>
2024-11-17 21:53:59 -0600

    replace material2 imports with material3

--- app-android/src/main/java/com/taushsampley/timer/organizer/Organizer.kt
+++ app-android/src/main/java/com/taushsampley/timer/organizer/Organizer.kt
@@ -6,11 +6,11 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Icon
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.material.icons.twotone.Folder
 import androidx.compose.material.icons.twotone.Timer
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
