95dc5ccf2812b870cdd46cfca6ec7f4333161748
Taush Sampley <taushsampley@gmail.com>
2024-11-23 19:38:12 -0600

    Create multiplatform view model stubs

---
+++ app-shared/src/commonMain/kotlin/org/pointyware/timer/shared/ui/ViewModelFactory.kt
@@ -0,0 +1,10 @@
+package org.pointyware.timer.shared.ui
+
+/**
+ *
+ */
+expect fun <T> viewModels(): ViewModel<T>
+
+expect abstract class ViewModel<T> {
+
+}
---
+++ app-shared/src/jsMain/kotlin/org/pointyware/timer/shared/ui/ViewModelFactory.js.kt
@@ -0,0 +1,12 @@
+package org.pointyware.timer.shared.ui
+
+/**
+ *
+ */
+actual fun <T> viewModels(): ViewModel<T> {
+    TODO("Not yet implemented")
+}
+
+actual abstract class ViewModel<T> {
+    // TODO: replace with android-platform view model typealias
+}
