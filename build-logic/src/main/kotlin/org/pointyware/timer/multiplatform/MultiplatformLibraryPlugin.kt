4084936b95b34aab2e4bf106282dcd1fb9ceab5b
Taush Sampley <taushsampley@gmail.com>
2024-11-30 18:25:10 -0600

    Add kotlin/jvm configuration project extensions

--- build-logic/src/main/kotlin/org/pointyware/timer/multiplatform/MultiplatformLibraryPlugin.kt
+++ build-logic/src/main/kotlin/org/pointyware/timer/multiplatform/MultiplatformLibraryPlugin.kt
@@ -3,6 +3,8 @@ package org.pointyware.timer.multiplatform
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.dependencies
+import org.pointyware.timer.configureKotlinAndroid
+import org.pointyware.timer.configureKotlinJvm
 import org.pointyware.timer.libs
 
 /**
@@ -10,13 +12,18 @@ import org.pointyware.timer.libs
  */
 class MultiplatformLibraryPlugin: Plugin<Project> {
     override fun apply(project: Project) {
-        with(project.plugins) {
-            apply("org.jetbrains.kotlin.multiplatform")
-            apply("com.android.library")
-        }
+        with(project) {
 
-        project.dependencies {
-            add("commonMainImplementation", project.libs.findLibrary("kotlinx.datetime"))
+            with(plugins) {
+                apply("org.jetbrains.kotlin.multiplatform")
+                apply("com.android.library")
+            }
+            configureKotlinAndroid()
+            configureKotlinJvm()
+
+            dependencies {
+                add("commonMainImplementation", project.libs.findLibrary("kotlinx.datetime"))
+            }
         }
     }
 }
