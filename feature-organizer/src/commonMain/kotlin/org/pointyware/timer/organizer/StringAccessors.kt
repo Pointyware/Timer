22b40d548fb37fbba2643ff74aecf2a863595f48
Taush Sampley <taushsampley@gmail.com>
2024-12-07 18:29:09 -0600

    create string accessors in feature-organizer root package

---
+++ feature-organizer/src/commonMain/kotlin/org/pointyware/timer/organizer/StringAccessors.kt
@@ -0,0 +1,9 @@
+package org.pointyware.timer.organizer
+
+import androidx.compose.runtime.Composable
+import org.jetbrains.compose.resources.stringResource
+
+/**
+ */
+val accNameCategory: String @Composable get() = stringResource(Res.string.acc_name_category)
+val accNameTask: String @Composable get() = stringResource(Res.string.acc_name_task)
