796d27b5b31c55386452e296b2befc3f177c1847
Taush Sampley <taushsampley@gmail.com>
2024-12-07 22:32:10 -0600

    Add day mode preview

--- app-android/src/main/java/com/taushsampley/timer/tasks/ui/Timer.kt
+++ app-android/src/main/java/com/taushsampley/timer/tasks/ui/Timer.kt
@@ -86,6 +86,7 @@ fun Timer(
 }
 
 @Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_NO)
 @Composable
 fun TimerPreview() {
     var time by remember { mutableStateOf(0) }
