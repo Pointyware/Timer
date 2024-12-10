983ea85be348bc550ddbf08b5456b637d519b3c9
Taush Sampley <taushsampley@gmail.com>
2024-12-09 21:17:15 -0600

    Migrate MetricsScreen to feature module

--- app-android/src/main/java/com/taushsampley/timer/metrics/MetricsScreen.kt
+++ app-android/src/main/java/com/taushsampley/timer/metrics/MetricsScreen.kt
@@ -1,29 +1,9 @@
 package com.taushsampley.timer.metrics
 
-import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import org.pointyware.timer.metrics.ui.RandomColorMap
+import org.pointyware.timer.metrics.ui.MetricsScreen
 import org.pointyware.timer.ui.theme.TimerTheme
-
-/**
- */
-@Composable
-fun MetricsScreen(
-    metricsViewModel: MetricsViewModel,
-    modifier: Modifier = Modifier
-) {
-    Column(modifier = modifier) {
-        val colorMap = remember { RandomColorMap<Int>() }
-        val metricsList by metricsViewModel.metricsList.collectAsState()
-
-        MetricsWheel(metricsList, colorMap = colorMap)
-    }
-}
 
 @Preview
 @Composable
---
+++ feature-metrics/src/commonMain/kotlin/org/pointyware/timer/metrics/ui/MetricsScreen.kt
@@ -0,0 +1,25 @@
+package org.pointyware.timer.metrics.ui
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import com.taushsampley.timer.metrics.MetricsWheel
+import org.pointyware.timer.metrics.viewmodels.IMetricsViewModel
+
+/**
+ */
+@Composable
+fun MetricsScreen(
+    metricsViewModel: IMetricsViewModel,
+    modifier: Modifier = Modifier
+) {
+    Column(modifier = modifier) {
+        val colorMap = remember { RandomColorMap<Int>() }
+        val metricsList by metricsViewModel.metricsList.collectAsState()
+
+        MetricsWheel(metricsList, colorMap = colorMap)
+    }
+}
