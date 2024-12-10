1cad1caa3f2ee719991bb83b7e59ede752b2b69a
Taush Sampley <taushsampley@gmail.com>
2024-12-09 20:56:24 -0600

    Move OrganizerScreen to feature module

--- feature-organizer/src/commonMain/kotlin/org/pointyware/timer/organizer/ui/OrganizerScreen.kt
+++ feature-organizer/src/commonMain/kotlin/org/pointyware/timer/organizer/ui/OrganizerScreen.kt
@@ -2,7 +2,10 @@ package org.pointyware.timer.organizer.ui
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import org.pointyware.timer.organizer.viewmodels.IOrganizerViewModel
 
 data class OrganizerScreenState(
     val elements: List<OrganizerElement>,
@@ -20,5 +23,20 @@ fun OrganizerScreen(
         elements = state.elements,
         onClickElement = onClickElement,
         modifier = Modifier.fillMaxSize(),
+    )
+}
+
+/**
+ */
+@Composable
+fun OrganizerScreen(
+    organizerViewModel: IOrganizerViewModel
+) {
+    val elements by organizerViewModel.elements.collectAsState()
+    OrganizerScreen(
+        state = OrganizerScreenState(
+            elements = elements
+        ),
+        onClickElement = organizerViewModel::onClickElement
     )
 }
