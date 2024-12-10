10b055b513826c324ddb7324b0dd49da2bc5a6eb
Taush Sampley <taushsampley@gmail.com>
2024-12-09 21:18:47 -0600

    Move metrics screen preview and view model into feature package

--- app-android/src/main/java/org/pointyware/timer/metrics/MetricsScreenPreview.kt
+++ app-android/src/main/java/org/pointyware/timer/metrics/MetricsScreenPreview.kt
@@ -1,13 +1,14 @@
-package com.taushsampley.timer.metrics
+package org.pointyware.timer.metrics
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import org.pointyware.timer.metrics.ui.MetricsScreen
+import org.pointyware.timer.metrics.viewmodels.MetricsViewModel
 import org.pointyware.timer.ui.theme.TimerTheme
 
 @Preview
 @Composable
-fun MetricsPreview() {
+fun MetricsScreenPreview() {
 
     val viewModel = MetricsViewModel()
 
