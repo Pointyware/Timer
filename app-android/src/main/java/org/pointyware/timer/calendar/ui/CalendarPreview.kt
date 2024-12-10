301a76a909aa7dfd750c2eddc1ca05e217036531
Taush Sampley <taushsampley@gmail.com>
2024-12-09 21:45:16 -0600

    Move calendar composable to feature module

---
+++ app-android/src/main/java/org/pointyware/timer/calendar/ui/CalendarPreview.kt
@@ -0,0 +1,82 @@
+package org.pointyware.timer.calendar.ui
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import org.pointyware.timer.R
+import org.pointyware.timer.calendar.viewmodels.DayMetrics
+import org.pointyware.timer.calendar.viewmodels.MonthMetrics
+import org.pointyware.timer.metrics.entities.CategoryMetric
+import org.pointyware.timer.metrics.entities.TaskMetric
+import org.pointyware.timer.ui.theme.TimerTheme
+
+@Preview
+@Composable
+fun CalendarDayPreview() {
+
+    val duration = 2500L
+    val dummyData = listOf(
+        TaskMetric(duration),
+        TaskMetric(duration),
+        TaskMetric(duration),
+        TaskMetric(duration),
+        CategoryMetric(
+            TaskMetric(duration),
+            TaskMetric(duration),
+            CategoryMetric(
+                TaskMetric(duration)
+            )
+        )
+    )
+
+    val blankDay = DayMetrics(null, null)
+    val blankMetrics = DayMetrics(1, null)
+    val metrics = DayMetrics(3, dummyData)
+
+    TimerTheme {
+        Row {
+            CalendarDay(blankDay, Modifier.weight(1f))
+            CalendarDay(blankMetrics, Modifier.weight(1f))
+            CalendarDay(metrics, Modifier.weight(1f))
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun CalendarPreview() {
+
+    fun dayMetrics(day: Int): DayMetrics
+    = if ((1..31).contains(day)) {
+        val data = listOf(
+            TaskMetric(5000),
+            TaskMetric(3000)
+        )
+        DayMetrics(day, data)
+    } else {
+        DayMetrics(null, null)
+    }
+
+    fun weekMetrics(startDay: Int)
+    = (0 until 7).map {
+        dayMetrics(startDay + it)
+    }
+
+    fun monthMetrics(weekCount: Int, offset: Int = 0)
+    = (0 until weekCount).map {
+        weekMetrics(it*7 + offset)
+    }
+
+    val metrics = remember {
+        MonthMetrics(
+            R.string.month_may, 2022, monthMetrics(6, -4)
+        )
+    }
+
+    TimerTheme {
+        Calendar(metrics)
+    }
+}
--- feature-calendar/src/commonMain/kotlin/org/pointyware/timer/calendar/ui/Calendar.kt
+++ feature-calendar/src/commonMain/kotlin/org/pointyware/timer/calendar/ui/Calendar.kt
@@ -1,6 +1,5 @@
-package com.taushsampley.timer.calendar
+package org.pointyware.timer.calendar.ui
 
-import androidx.annotation.StringRes
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -14,26 +13,27 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringArrayResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import org.pointyware.timer.R
+import org.jetbrains.compose.resources.stringArrayResource
+import org.jetbrains.compose.resources.stringResource
+import org.pointyware.timer.calendar.Res
+import org.pointyware.timer.calendar.acc_next_month
+import org.pointyware.timer.calendar.acc_previous_month
+import org.pointyware.timer.calendar.acc_select_month
+import org.pointyware.timer.calendar.loading
+import org.pointyware.timer.calendar.months
 import org.pointyware.timer.calendar.viewmodels.DayMetrics
 import org.pointyware.timer.calendar.viewmodels.MonthMetrics
-import org.pointyware.timer.metrics.entities.CategoryMetric
-import org.pointyware.timer.metrics.entities.TaskMetric
+import org.pointyware.timer.calendar.weekdays_short
 import org.pointyware.timer.metrics.ui.MetricsWheel
 import org.pointyware.timer.ui.theme.TimerIconsAutoMirrored
-import org.pointyware.timer.ui.theme.TimerTheme
 
 @Composable
 fun MonthHeading(
-    @StringRes title: Int,
+    title: String,
     modifier: Modifier = Modifier,
     onClickHeading: () -> Unit = {},
     onClickPrevious: (() -> Unit)? = null,
@@ -46,16 +46,16 @@ fun MonthHeading(
             IconButton(onClick = onClickPrevious ?: {}, enabled = onClickPrevious != null) {
                 Icon(
                     TimerIconsAutoMirrored.ArrowBack,
-                    contentDescription = stringResource(R.string.acc_previous_month)
+                    contentDescription = stringResource(Res.string.acc_previous_month)
                 )
             }
             Text(
-                text = stringResource(id = title),
+                text = title,
                 style = MaterialTheme.typography.headlineMedium,
                 modifier = Modifier
                     .clickable(
                         onClick = onClickHeading,
-                        onClickLabel = stringResource(R.string.acc_select_month)
+                        onClickLabel = stringResource(Res.string.acc_select_month)
                     )
                     .weight(1f),
                 textAlign = TextAlign.Center
@@ -63,13 +63,13 @@ fun MonthHeading(
             IconButton(onClick = onClickNext ?: {}, enabled = onClickNext != null) {
                 Icon(
                     TimerIconsAutoMirrored.ArrowForward,
-                    contentDescription = stringResource(R.string.acc_next_month)
+                    contentDescription = stringResource(Res.string.acc_next_month)
                 )
             }
         }
 
         Row {
-            stringArrayResource(id = R.array.weekDaysShort).forEach {
+            stringArrayResource(Res.array.weekdays_short).forEach {
                 Text(
                     text = it,
                     style = MaterialTheme.typography.titleSmall,
@@ -79,6 +79,13 @@ fun MonthHeading(
             }
         }
     }
+}
+
+@Composable
+fun MonthTitle(
+    index: Int
+): String {
+    return stringArrayResource(Res.array.months)[index]
 }
 
 /**
@@ -92,7 +99,9 @@ fun Calendar(
 
     Surface {
         Column(modifier = modifier) {
-            MonthHeading(title = metrics?.month ?: R.string.loading)
+            val title = metrics?.let { MonthTitle(index = it.month) }
+                ?: stringResource(Res.string.loading)
+            MonthHeading(title = title)
             metrics?.also {
                 it.metrics.forEach { week ->
 
@@ -122,74 +131,5 @@ fun CalendarDay(
         metrics.day?.let {
             Text(text = "$it")
         }
-    }
-}
-
-@Preview
-@Composable
-fun CalendarDayPreview() {
-
-    val duration = 2500L
-    val dummyData = listOf(
-        TaskMetric(duration),
-        TaskMetric(duration),
-        TaskMetric(duration),
-        TaskMetric(duration),
-        CategoryMetric(
-            TaskMetric(duration),
-            TaskMetric(duration),
-            CategoryMetric(
-                TaskMetric(duration)
-            )
-        )
-    )
-
-    val blankDay = DayMetrics(null, null)
-    val blankMetrics = DayMetrics(1, null)
-    val metrics = DayMetrics(3, dummyData)
-
-    TimerTheme {
-        Row {
-            CalendarDay(blankDay, Modifier.weight(1f))
-            CalendarDay(blankMetrics, Modifier.weight(1f))
-            CalendarDay(metrics, Modifier.weight(1f))
-        }
-    }
-}
-
-
-@Preview(showBackground = true)
-@Composable
-fun CalendarPreview() {
-
-    fun dayMetrics(day: Int): DayMetrics
-    = if ((1..31).contains(day)) {
-        val data = listOf(
-            TaskMetric(5000),
-            TaskMetric(3000)
-        )
-        DayMetrics(day, data)
-    } else {
-        DayMetrics(null, null)
-    }
-
-    fun weekMetrics(startDay: Int)
-    = (0 until 7).map {
-        dayMetrics(startDay + it)
-    }
-
-    fun monthMetrics(weekCount: Int, offset: Int = 0)
-    = (0 until weekCount).map {
-        weekMetrics(it*7 + offset)
-    }
-
-    val metrics = remember {
-        MonthMetrics(
-            R.string.month_may, 2022, monthMetrics(6, -4)
-        )
-    }
-
-    TimerTheme {
-        Calendar(metrics)
     }
 }
