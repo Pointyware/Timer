1068ee7408552a9cddf13662e9526928bd836864
Taush Sampley <taushsampley@gmail.com>
2024-12-09 20:44:47 -0600

    Move screen composable to multiplatform feature module

--- app-android/src/main/java/com/taushsampley/timer/tasks/ui/TimerScreen.kt
+++ app-android/src/main/java/com/taushsampley/timer/tasks/ui/TimerScreen.kt
@@ -2,38 +2,10 @@ package com.taushsampley.timer.tasks.ui
 
 import android.content.res.Configuration
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.tooling.preview.Preview
 import org.pointyware.timer.tasks.TimerScreen
 import org.pointyware.timer.tasks.TimerScreenState
-import org.pointyware.timer.tasks.ui.ITimerViewModel
 import org.pointyware.timer.ui.theme.TimerTheme
-
-@Composable
-fun TimerScreen(
-    timerViewModel: ITimerViewModel
-) {
-    val currentTime by timerViewModel.time.collectAsState()
-    val isRunning by timerViewModel.isRunning.collectAsState()
-    val recordingList by timerViewModel.recordings.collectAsState()
-    val title by timerViewModel.taskTitle.collectAsState()
-    val taskList by timerViewModel.taskList.collectAsState()
-    val selectedTask by timerViewModel.selectedTask.collectAsState()
-    TimerScreen(
-        state = TimerScreenState(
-            currentTime = currentTime,
-            isRunning = isRunning,
-            recordingList = recordingList,
-            title = title,
-            taskList = taskList,
-            selectedTask = selectedTask
-        ),
-        onToggleTimer = timerViewModel::toggleTimer,
-        onTitleChanged = timerViewModel::titleChanged,
-        onTaskSelected = timerViewModel::selectTask
-    )
-}
 
 @Preview(showBackground = true, name = "Timer Light")
 @Preview(showBackground = true, name = "Timer Dark", uiMode = Configuration.UI_MODE_NIGHT_YES)
