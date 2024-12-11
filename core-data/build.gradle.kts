e0e8e800322564dd57a3ddbddff17d93c3c85119
Taush Sampley <taushsampley@gmail.com>
2024-12-10 20:51:02 -0600

    Transfer SQLDelight configuration to core-data

--- core-data/build.gradle.kts
+++ core-data/build.gradle.kts
@@ -1,14 +1,37 @@
+import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 
 plugins {
     id("pointyware.timer.multiplatform.library")
+    alias(libs.plugins.sqlDelight)
 }
 
 kotlin {
+
+    @OptIn(ExperimentalKotlinGradlePluginApi::class)
+    compilerOptions {
+        freeCompilerArgs = listOf("-Xexpect-actual-classes")
+    }
 
     sourceSets {
         val commonMain by getting {
             dependencies {
                 api(projects.coreEntities)
+
+                implementation(libs.koin.core)
+            }
+        }
+
+        val desktopMain by getting {
+            dependencies {
+                implementation(libs.sqlDelight.jvm)
+            }
+        }
+
+        val androidMain by getting {
+            dependencies {
+                implementation(libs.sqlDelight.android)
+
+                implementation(libs.koin.android)
             }
         }
     }
@@ -16,4 +39,12 @@ kotlin {
 
 android {
     namespace = "org.pointyware.timer.data"
+}
+
+sqldelight {
+    databases {
+        create("TimerDatabase") {
+            packageName = "org.pointyware.timer.data.db"
+        }
+    }
 }
