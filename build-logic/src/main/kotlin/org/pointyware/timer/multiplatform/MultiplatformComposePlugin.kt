44e7a33b46ab2d771b443aec6bdb828a56ab07dd
Taush Sampley <taushsampley@gmail.com>
2024-12-13 18:10:10 -0600

    stub platform extensions

--- build-logic/src/main/kotlin/org/pointyware/timer/multiplatform/MultiplatformComposePlugin.kt
+++ build-logic/src/main/kotlin/org/pointyware/timer/multiplatform/MultiplatformComposePlugin.kt
@@ -1,7 +1,13 @@
 package org.pointyware.timer.multiplatform
 
+import com.android.build.api.dsl.ApplicationExtension
+import com.android.build.api.dsl.LibraryExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.plugins.JavaPluginExtension
+import org.gradle.kotlin.dsl.apply
+import org.gradle.kotlin.dsl.configure
+import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 
 /**
  *
@@ -9,9 +15,33 @@ import org.gradle.api.Project
 class MultiplatformComposePlugin: Plugin<Project> {
     override fun apply(target: Project) {
         with(target) {
-            with(plugins) {
-                apply("org.jetbrains.compose")
-                apply("org.jetbrains.kotlin.plugin.compose")
+            apply(plugin = "org.jetbrains.compose")
+            apply(plugin = "org.jetbrains.kotlin.plugin.compose")
+
+            when {
+                pluginManager.hasPlugin("org.jetbrains.kotlin.multiplatform") -> {
+                    configure<KotlinMultiplatformExtension> {
+
+                    }
+                }
+
+                pluginManager.hasPlugin("org.jetbrains.kotlin.jvm") -> {
+                    configure<JavaPluginExtension> {
+
+                    }
+                }
+
+                pluginManager.hasPlugin("com.android.library") -> {
+                    configure<LibraryExtension> {
+
+                    }
+                }
+
+                pluginManager.hasPlugin("com.android.application") -> {
+                    configure<ApplicationExtension> {
+
+                    }
+                }
             }
         }
     }
