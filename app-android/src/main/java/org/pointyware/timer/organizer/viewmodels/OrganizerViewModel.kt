169ad3127283f63185bfa9229404a398eb78ade1
Taush Sampley <taushsampley@gmail.com>
2024-12-16 21:59:44 -0600

    Add hilt view model annotations

--- app-android/src/main/java/org/pointyware/timer/organizer/viewmodels/OrganizerViewModel.kt
+++ app-android/src/main/java/org/pointyware/timer/organizer/viewmodels/OrganizerViewModel.kt
@@ -1,14 +1,17 @@
 package org.pointyware.timer.organizer.viewmodels
 
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import org.pointyware.timer.organizer.ui.OrganizerElement
+import javax.inject.Inject
 
 /**
  *
  */
-class OrganizerViewModel(
+@HiltViewModel
+class OrganizerViewModel @Inject constructor(
     // TODO: inject repository and interactors
 ): ViewModel(), IOrganizerViewModel {
 
