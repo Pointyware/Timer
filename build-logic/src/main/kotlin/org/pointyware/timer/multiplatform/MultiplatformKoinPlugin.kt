8e5b1264bb0e17ce432fbeee75c5419963f056e0
Taush Sampley <taushsampley@gmail.com>
2024-12-16 21:49:34 -0600

    Create multiplatform koin plugin

---
+++ build-logic/src/main/kotlin/org/pointyware/timer/multiplatform/MultiplatformKoinPlugin.kt
@@ -0,0 +1,44 @@
+package org.pointyware.timer.multiplatform
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.dependencies
+import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
+import org.pointyware.timer.libs
+
+/**
+ *
+ */
+class MultiplatformKoinPlugin: Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+            when {
+                pluginManager.hasPlugin("org.jetbrains.kotlin.multiplatform") -> {
+                    configure<KotlinMultiplatformExtension> {
+                        sourceSets.apply {
+                            commonMain.dependencies {
+                                implementation(libs.findLibrary("koin-core"))
+                            }
+                            androidMain.dependencies {
+                                implementation(libs.findLibrary("koin-android"))
+                            }
+                        }
+                    }
+                }
+
+                pluginManager.hasPlugin("org.jetbrains.kotlin.jvm") -> {
+                    dependencies {
+                        add("implementation", libs.findLibrary("koin-core"))
+                    }
+                }
+
+                pluginManager.hasPlugin("org.jetbrains.kotlin.android") -> {
+                    dependencies {
+                        add("implementation", libs.findLibrary("koin-android"))
+                    }
+                }
+            }
+        }
+    }
+}
