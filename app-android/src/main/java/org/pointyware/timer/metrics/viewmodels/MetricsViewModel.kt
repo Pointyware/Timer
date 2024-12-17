d2df78c0189a49dd9e2e5251a10101f0b59a9d97
Taush Sampley <taushsampley@gmail.com>
2024-12-16 21:59:44 -0600

    Add hilt view model annotations

--- app-android/src/main/java/org/pointyware/timer/metrics/viewmodels/MetricsViewModel.kt
+++ app-android/src/main/java/org/pointyware/timer/metrics/viewmodels/MetricsViewModel.kt
@@ -1,13 +1,18 @@
 package org.pointyware.timer.metrics.viewmodels
 
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import org.pointyware.timer.metrics.entities.CategoryMetric
 import org.pointyware.timer.metrics.entities.Metric
 import org.pointyware.timer.metrics.entities.TaskMetric
+import javax.inject.Inject
 
-class MetricsViewModel: ViewModel(), IMetricsViewModel {
+@HiltViewModel
+class MetricsViewModel @Inject constructor(
+
+): ViewModel(), IMetricsViewModel {
 
     private val _metricsList = MutableStateFlow<List<Metric>>(listOf())
     override val metricsList: StateFlow<List<Metric>> = _metricsList
