50848236a2ddf5d13146a092dbd714a1e9ad0090
Taush Sampley <taushsampley@gmail.com>
2024-12-16 22:54:52 -0600

    remove koin repository construction in application

--- app-android/src/main/java/org/pointyware/timer/TimerApplication.kt
+++ app-android/src/main/java/org/pointyware/timer/TimerApplication.kt
@@ -4,10 +4,6 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.GlobalContext.startKoin
-import org.koin.mp.KoinPlatform.getKoin
-import org.pointyware.timer.data.TaskRepository
-import org.pointyware.timer.data.TaskRepositoryImpl
-import org.pointyware.timer.data.db.DriverFactory
 import org.pointyware.timer.data.di.coreDataModule
 
 /**
@@ -15,8 +11,6 @@ import org.pointyware.timer.data.di.coreDataModule
  */
 @HiltAndroidApp
 class TimerApplication: Application() {
-
-    lateinit var repository: TaskRepository
 
     override fun onCreate() {
         super.onCreate()
@@ -27,8 +21,5 @@ class TimerApplication: Application() {
                 coreDataModule(),
             )
         }
-        val koin = getKoin()
-        val driver = koin.get<DriverFactory>()
-        repository = TaskRepositoryImpl(driver)
     }
 }
