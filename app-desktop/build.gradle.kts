0266b0882481ccccd390782c4a757e8ad8d9c904
Taush Sampley <taushsampley@gmail.com>
2024-12-23 22:01:03 -0600

    remove redundant application plugin

--- app-desktop/build.gradle.kts
+++ app-desktop/build.gradle.kts
@@ -4,10 +4,7 @@ plugins {
     alias(libs.plugins.kotlinJvm)
     id("org.pointyware.timer.multiplatform.compose")
     id("org.pointyware.timer.multiplatform.koin")
-    application
 }
-
-version = libs.versions.app.get()
 
 kotlin {
     jvmToolchain(17)
