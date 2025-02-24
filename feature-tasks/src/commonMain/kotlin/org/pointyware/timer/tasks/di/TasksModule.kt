1be43440c5e2f0ec71a7946efc93b18e11d7c041
Taush Sampley <taushsampley@gmail.com>
2025-02-24 06:32:04 -0600

    Provide TimerViewModel implementation in koin dependencies

--- feature-tasks/src/commonMain/kotlin/org/pointyware/timer/tasks/di/TasksModule.kt
+++ feature-tasks/src/commonMain/kotlin/org/pointyware/timer/tasks/di/TasksModule.kt
@@ -1,10 +1,17 @@
 package org.pointyware.timer.tasks.di
 
+import org.koin.core.module.dsl.bind
+import org.koin.core.module.dsl.factoryOf
 import org.koin.dsl.module
+import org.pointyware.timer.tasks.viewmodels.ITimerViewModel
+import org.pointyware.timer.tasks.viewmodels.TimerViewModelImpl
 
 /**
  *
  */
 fun tasksModule() = module {
 
+    factoryOf(::TimerViewModelImpl) {
+        bind<ITimerViewModel>()
+    }
 }
