1bb7edd28c67f435af9d205a2c68e48bc6b81b05
Taush Sampley <taushsampley@gmail.com>
2025-02-26 06:51:19 -0600

    connect missing feature koin modules

--- app-shared/src/commonMain/kotlin/org/pointyware/timer/shared/di/SharedModule.kt
+++ app-shared/src/commonMain/kotlin/org/pointyware/timer/shared/di/SharedModule.kt
@@ -1,9 +1,12 @@
 package org.pointyware.timer.shared.di
 
 import org.koin.dsl.module
+import org.pointyware.timer.calendar.di.calendarModule
 import org.pointyware.timer.data.di.coreDataModule
 import org.pointyware.timer.di.coreInteractorsModule
 import org.pointyware.timer.metrics.di.metricsModule
+import org.pointyware.timer.organizer.di.organizerModule
+import org.pointyware.timer.tasks.di.tasksModule
 
 /**
  *
@@ -12,6 +15,10 @@ fun sharedModule() = module {
     includes(
         coreDataModule(),
         coreInteractorsModule(),
+
         metricsModule(),
+        calendarModule(),
+        tasksModule(),
+        organizerModule()
     )
 }
